d1bfac01eed1ff21f834651178272d9a663223bf
ninadnale <ninadnale@gmail.com>
2024-03-13 23:42:26 -0700

    Airflow scheduler for extract_stats.py

--- cloud_composer_DAG.py
+++ cloud_composer_DAG.py
@@ -23,5 +23,5 @@ dag = DAG('fetch_cricket_stats',
 with dag:
     run_script_task = BashOperator(
         task_id='run_script',
-        bash_command='python /home/airflow/gcs/dags/scripts/extract_and_push_gcs.py',
+        bash_command='python /home/airflow/gcs/dags/scripts/extract_stats.py',
     )
