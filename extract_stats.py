96feefd971fafcd72b0c5de5d4d137ab11d1accd
ninadnale <ninadnale@gmail.com>
2024-03-13 23:41:08 -0700

    added push_to_GCS function

--- extract_stats.py
+++ extract_stats.py
@@ -1,34 +1,50 @@
 import requests
 import csv
+from google.cloud import storage
 
-url = "https://cricbuzz-cricket.p.rapidapi.com/stats/v1/rankings/batsmen"
+class extractBatsmenRanking():
+    def __init__(self) -> None:
+        self.url = "https://cricbuzz-cricket.p.rapidapi.com/stats/v1/rankings/batsmen"
+        self.querystring = {"formatType":"odi"}
+        self.headers = {
+            "X-RapidAPI-Key": "e3f4345048msh0c98a793249d42bp1e71c4jsn9ebda9676726",
+            "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com"
+        }
 
-querystring = {"formatType":"odi"}
+    def extract_stats(self) -> None:
+        response = requests.get(self.url, headers=self.headers, params=self.querystring)
 
-headers = {
-	"X-RapidAPI-Key": "e3f4345048msh0c98a793249d42bp1e71c4jsn9ebda9676726",
-	"X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com"
-}
+        if response.status_code == 200:
+            data = response.json().get('rank', [])
+            csv_filename = 'batsmen_odi_rankings.csv'
 
-response = requests.get(url, headers=headers, params=querystring)
+            if data:
+                attributes = ['rank', 'name', 'country']
 
+                with open(csv_filename, 'w', newline='', encoding='utf-8') as csv_file:
+                    writer = csv.DictWriter(csv_file, fieldnames=attributes)
+                    # writer.writeheader()
+                    for record in data:
+                        writer.writerow({attribute: record.get(attribute) for attribute in attributes})
+                
+                print(f"Data written to {csv_filename}")
+                self.push_to_gcs(csv_filename)
+            else:
+                print("No data available from API!")
 
-if response.status_code == 200:
-    data = response.json().get('rank', [])
-    csv_filename = 'batsmen_odi_rankings.csv'
+        else:
+            print(f"Failed to fetch data from API! Status code: {response.status_code}")
 
-    if data:
-        attributes = ['rank', 'name', 'country']
+    def push_to_gcs(self, csv_filename):
+        bucket_name = 'cricket-statistics-batsmen-rankings'
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        destination_blob_name = f'{csv_filename}'  # The path to store in GCS
 
-        with open(csv_filename, 'w', newline='', encoding='utf-8') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=attributes)
-            # writer.writeheader()
-            for record in data:
-                writer.writerow({attribute: record.get(attribute) for attribute in attributes})
-        
-        print(f"Data written to {csv_filename}")
-    else:
-        print("No data available from API!")
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(csv_filename)
 
-else:
-    print(f"Failed to fetch data from API! Status code: {response.status_code}")
+        print(f"File {csv_filename} uploaded to GCS bucket {bucket_name} as {destination_blob_name}")
+
+if __name__ =="__main__":
+    extractBatsmenRanking().extract_stats()
